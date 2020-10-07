76ff3504d72475eb2e46461243251aac27d21282
Mihail155 <danilnasirbanov@gmail.com>
2020-10-07 16:09:35 +0300

    Updates

--- precache-manifest.d952fd33a079d59785b256c493ca3028.js
+++ precache-manifest.d952fd33a079d59785b256c493ca3028.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3e36f275df0a3bb1ddd41b2c9a308c8b",
+    "revision": "ad9cfd72feccae7ec3ba3e65cf48d98a",
     "url": "/Constructor_Game/index.html"
   },
   {
-    "revision": "9cb36cd17610b7a4cdf6",
+    "revision": "6260e63130530c684223",
     "url": "/Constructor_Game/static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "4f7901aa89a4a68aa3d2",
+    "revision": "348a3bea231c452445a9",
     "url": "/Constructor_Game/static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "9cb36cd17610b7a4cdf6",
-    "url": "/Constructor_Game/static/js/2.99381cc0.chunk.js"
+    "revision": "6260e63130530c684223",
+    "url": "/Constructor_Game/static/js/2.ec667992.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "/Constructor_Game/static/js/2.99381cc0.chunk.js.LICENSE.txt"
+    "url": "/Constructor_Game/static/js/2.ec667992.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f7901aa89a4a68aa3d2",
-    "url": "/Constructor_Game/static/js/main.a14a620f.chunk.js"
+    "revision": "348a3bea231c452445a9",
+    "url": "/Constructor_Game/static/js/main.366dadae.chunk.js"
   },
   {
     "revision": "27ac5ceea49a11460d0d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Constructor_Game/precache-manifest.84a70058517420a079e390d9ee5745a5.js"
+  "/Constructor_Game/precache-manifest.d952fd33a079d59785b256c493ca3028.js"
 );
 
 self.addEventListener('message', (event) => {
